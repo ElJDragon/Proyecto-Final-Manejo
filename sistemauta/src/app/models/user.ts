0f31f11d8b23e0705357809e263fd5c6e477e757
Josue Guevara <el.jdragon@gmail.com>
2025-06-04 20:24:15 -0500

    Creacion de modelos

---
+++ sistemauta/src/app/models/user.ts
@@ -0,0 +1,14 @@
+export class User{
+constructor (public id:number,
+			 public Nombre:string,
+      		 public Email:string,
+      		 public Password:string,
+      		 public Password2:string,
+      		 public image:string,
+      		 public role:string
+			 )
+{
+
+}
+
+}
