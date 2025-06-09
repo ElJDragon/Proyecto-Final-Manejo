992a94df9d2b5bd4934f457111c95f77936f70db
Josue Guevara <el.jdragon@gmail.com>
2025-06-09 10:16:08 -0500

    Generacon automatica y modificacion d elos modelos con copia y pega de la carpeta vendor

---
+++ sistemauta/src/app/models/eventousuario.ts
@@ -0,0 +1,14 @@
+export class Eventousuario{
+constructor (public CODIGO:string,
+			 public USUARIO:string,
+			 public CURSO:string,
+			 public CALIFICACION:string,
+			 public ASISTENCIA:string,
+			 public FECHA:string
+			 
+			 )
+{
+
+}
+
+}
